807c098427be018e0bf082c31f473a79d68134f5
savesj <jesuslares.20@hotmail.com>
2022-08-24 10:30:01 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,24 @@
-const router = require('express').Router()
+const router = require("express").Router()
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     let places = [{
-  name: 'H-Thai-ML',
-  city: 'Seattle',
-  state: 'WA',
-  cuisines: 'Thai, Pan-Asian',
-  pic: '/images/salmonPlate.jpg'
+  name: "H-Thai-ML",
+  city: "Seattle",
+  state: "WA",
+  cuisines: "Thai, Pan-Asian",
+  pic: "/images/salmonPlate.jpg"
 }, {
-  name: 'Coding Cat Cafe',
-  city: 'Phoenix',
-  state: 'AZ',
-  cuisines: 'Coffee, Bakery',
-  pic: '/images/barbecue.jpg'
-}]
+  name: "Coding Cat Cafe",
+  city: "Phoenix",
+  state: "AZ",
+  cuisines: "Coffee, Bakery",
+  pic: "/images/barbecue.jpg"
+  },
+];
+    res.render("places/index", {places})
+});
+ router.get("/new", (req, res)=>{
+      res.render("places/new");
+    });
 
-    res.render('places/index', {places})
-})
-
-module.exports = router
+module.exports = router;
