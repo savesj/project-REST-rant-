3cdd843408a3e267028313d2affb74a884dfcaa0
savesj <jesuslares.20@hotmail.com>
2022-08-24 13:36:52 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,28 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const places = require("../models/places.js")
 
+//GET /places
 router.get("/", (req, res) => {
-    let places = [{
-  name: "H-Thai-ML",
-  city: "Seattle",
-  state: "WA",
-  cuisines: "Thai, Pan-Asian",
-  pic: "/images/salmonPlate.jpg"
-}, {
-  name: "Coding Cat Cafe",
-  city: "Phoenix",
-  state: "AZ",
-  cuisines: "Coffee, Bakery",
-  pic: "/images/barbecue.jpg"
-  },
-];
-    res.render("places/index", {places})
+res.render("places/index", {places})
 });
+
  router.get("/new", (req, res)=>{
       res.render("places/new");
     });
 router.post("/", (req, res) => {
-  console.log(req.body)
-  res.send("POST /places");
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400"
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body)
+  res.redirect("/places");
 });
 
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: "/images/salmonPlate.jpg"
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: "/images/barbecue.jpg"
+},
+];
