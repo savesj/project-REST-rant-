eaff1e5d5c53fb56bdfb02ca58e58893f307af82
savesj <jesuslares.20@hotmail.com>
2022-08-24 22:17:54 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -41,6 +41,19 @@ router.get('/:id', (req, res) => {
   }
 });   
 
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
+});
+
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -55,5 +68,31 @@ router.delete('/:id', (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+})
 
 module.exports = router;
---
+++ views/places/edit.jsx
@@ -0,0 +1,44 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form (data) {
+    return (
+        <Def>
+            <main>
+                <h1>Edit</h1>
+            </main>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                    
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" defaultValue={data.place.name} required />
+                    </div>
+                        <div className="form-group col-sm-6">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" type="url" id="pic" name="pic" defaultValue={data.place.pic} />
+                    </div>
+                        <div className="form-group col-sm-6">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" defaultValue={data.place.city} />
+                    </div>
+                        <div className="form-group col-sm-6">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" defaultValue={data.place.state} />
+                    </div>
+                        <div className="form-group col-sm-6">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input className="form-control" id="cuisines" name="cuisines" defaultValue={data.place.cuisines} required />
+                    </div>
+                    <input type="submit" value="Add Place" />
+
+                   </div>
+                
+                </form>
+                
+           
+        </Def>
+    )
+}
+
+module.exports = edit_form
